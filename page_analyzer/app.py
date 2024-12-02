a4c09df128fc0d539672261d7215aa3261ed0d78
artem <artem_smirnov88@mail.ru>
2024-12-02 15:50:15 +0300

    optimize imports

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,8 +9,8 @@ from requests.exceptions import RequestException
 
 from page_analyzer.utils import normalize, validate
 from .database import (add_to_url_checks, add_to_urls,
-                       connection, get_url_by_id, get_url_by_name, get_url_checks, \
-    get_urls)
+                       connection, get_url_by_id,
+                       get_url_by_name, get_url_checks, get_urls)
 from .html import get_seo_content
 
 load_dotenv()
