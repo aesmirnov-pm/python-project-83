51db701ddf5b80faae51e9e97a440525cbdb28fe
artem <artem_smirnov88@mail.ru>
2024-12-04 11:48:10 +0300

    update imports

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,19 @@
 import logging
 import os
 
-import requests
 from dotenv import load_dotenv
-from flask import (abort, flash, Flask, redirect, render_template, request, url_for)
+from flask import (
+    Flask, abort, flash, redirect, render_template, request, url_for
+)
+import requests
 from requests.exceptions import RequestException
 
-from page_analyzer.utils import normalize, validate
 from .database import (
     add_to_url_checks, add_to_urls, connection, get_url_by_id,
     get_url_by_name, get_url_checks, get_urls
 )
 from .html import get_seo_content
+from page_analyzer.utils import normalize, validate
 
 load_dotenv()
 
